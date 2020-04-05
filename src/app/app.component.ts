841a17a25394afdaea8611a61c00ee5f072f9ac0
scarjo11 <sezzle-wave@live.fr>
2020-04-05 11:25:31 +0200

    Implémentation de la fonction SignUp()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves-project';
+
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCnvqUv-yt0FsEFCK91NMAfqypNlN0kRs8",
+      authDomain: "bookshelves-project-f8012.firebaseapp.com",
+      databaseURL: "https://bookshelves-project-f8012.firebaseio.com",
+      projectId: "bookshelves-project-f8012",
+      storageBucket: "bookshelves-project-f8012.appspot.com",
+      messagingSenderId: "654323309621",
+      appId: "1:654323309621:web:5af5e17372d878d276a57c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
